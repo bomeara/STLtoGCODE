e734134bf4fc2363a2938f864448a10562d77382
Brian O'Meara <omeara.brian@gmail.com>
2020-12-10 15:12:40 -0500

    now does outer margin, too

--- R/stg.R
+++ R/stg.R
@@ -49,13 +49,14 @@ stl_dimensions <- function(stl) {
 #' @param zero_position_xy can be "bottomleft", "center", or "current".
 #' @param zero_position_z can be "top", "bottom", or "current".
 #' @param set_max_dimension if not NULL, rescales the object so the maximum width or length is this size
+#' @param z_scale multiply the z by a factor. If greater than 1, relief is increased, if less than 1, the object is flattened
 #' @param verbose if TRUE, report on progress
 #' @return A matrix with x, y, and z points; x and y are evenly spaced grid
 #' @export
 #' @examples
 #' cat_paw <- stl_load("~/Downloads/catsnowscaled.stl")
 #' cat_paw_reg <- stl_regularize(cat_paw)
-stl_regularize <- function(stl, fineness=10, nmax=20, zero_position_xy="bottomleft", zero_position_z="top", set_max_dimension=NULL, verbose=TRUE) {
+stl_regularize <- function(stl, fineness=10, nmax=20, zero_position_xy="bottomleft", zero_position_z="top", set_max_dimension=NULL, z_scale=1, verbose=TRUE) {
 
 
 	
@@ -74,6 +75,7 @@ stl_regularize <- function(stl, fineness=10, nmax=20, zero_position_xy="bottomle
 		stl[,"z"] <- stl[,"z"]-min(stl[,"z"])
 	}
 
+	stl[,"z"] <- z_scale*stl[,"z"]
 
 	ranges <- as.data.frame(stl_dimensions(stl))
 	xwidth <- abs(diff(ranges$x))
@@ -128,9 +130,10 @@ stl_regularize <- function(stl, fineness=10, nmax=20, zero_position_xy="bottomle
 #' 
 #' The defaults work for my machine (a MySweety 3018) but you should check this for yours. You can look at the final code; the site https://www.cnccookbook.com/g-code-m-code-reference-list-cnc-mills/ will explain what codes mean. The important things to check are the three speeds, the units you want to use, and the stepover_height. spin_speed is how quickly to rotate the spindle. horizontal_speed is how quickly to move left and right and forward and back while cutting through material: too fast and it might be too hard for your machine (or snap your bit), too slow and the run takes longer. Vertical is the same but for moving into and out of the material. stepover_height is how much clearance to leave above obstacles -- make sure your spindle can go up at least this much above its starting position. stepdown_depth is how much lower to go into the material each pass.
 #' 
+#' 
 #' Remember, this code is not guaranteed to work! And it is code telling an expensive robot how to move very quickly moving bits of metal across resisting surfaces. If it goes wrong, it can go VERY wrong: damaging your machine, damaging the materials, setting things on fire, hurting people. Use at your own risk, and I strongly suggesting seeking out better options (like FreeCAD) if they are available to you. The settings seem to work for my machine, but they could be too fast or slow for yours, make sure you don't try to engrave something too large, etc. You can run your code at https://nraynaud.github.io/webgcode/ to see what it will look like and how long it will take.
 #' 
-#' The algorithm I use here is very basic -- essentially moving back and forth like a farmer plowing their field. There are far more clever algorithms like spiraling, climbing cuts, etc. This also does not focus on the tool size: basically, it moves the bottom tip of the tool back and forth along each line specified in the object, moving a little bit downward each time. 
+#' The algorithm I use here is very basic. It first does the outer margin of the object: the left, right, top, and bottom of the area to carve in. Then it essentially moving back and forth like a farmer plowing their field within this region, moving down by a fixed step size each time. Finally, it does a smoothing pass where the tool can move up and down while moving left and right or forward and back. There are far more clever algorithms like spiraling, climbing cuts, etc. This also does not focus on the tool size: basically, it moves the bottom tip of the tool back and forth along each line specified in the object, moving a little bit downward each time. 
 #' 
 #' @param stl object from stl_load(), ideally after running through stl_regularize
 #' @param gcode_file path to use to store gcode output
@@ -149,7 +152,7 @@ stl_regularize <- function(stl, fineness=10, nmax=20, zero_position_xy="bottomle
 #' cat_paw <- stl_load("~/Downloads/catsnowscaled.stl")
 #' cat_paw_reg <- stl_regularize(cat_paw, set_max_dimension=2)
 #' stl_generate_gcode(cat_paw_reg, gcode_file="~/Downloads/cat.nc")
-stl_generate_gcode <- function(stl, gcode_file='gcode.nc', spin_speed=12000, horizontal_speed=30, vertical_speed=9, stepover_height = 0.2, stepdown_depth=0.02, final_pass_smoothly=TRUE, final_pass_smoothly_height=NULL, unit="inches", unit_precision=5, verbose=TRUE) {
+stl_generate_gcode <- function(stl, gcode_file='gcode.nc', spin_speed=12000, horizontal_speed=30, vertical_speed=9, stepover_height = 0.05, stepdown_depth=0.03, final_pass_smoothly=TRUE, final_pass_smoothly_height=NULL, unit="inches", unit_precision=5, verbose=TRUE) {
 	#we need to turn off scientific notation so there aren't odd errors in output
 	original_scipen <- getOption('scipen')
 	options(scipen = 999)
@@ -174,13 +177,67 @@ stl_generate_gcode <- function(stl, gcode_file='gcode.nc', spin_speed=12000, hor
 	cat(paste0("G1 Z", stepover_height, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #let's slowly go up to clear the piece
 	cat(paste0("G0 X", starting_x, " Y", starting_y, "\n"), file=gcode_file, append=TRUE) #shoot over to the starting position
 
+	# first do loops on the outer margin
+
+	if(verbose) {
+		print(paste0("Computing passes to do outer margin"))
+	}
+
+	outer_margins <- list(
+		left_col = stl[stl[,"x"]==x_positions[1],],
+		right_col = stl[stl[,"x"]==x_positions[length(x_positions)],],
+		bottom_row = stl[stl[,"y"]==y_positions[length(y_positions)], ],
+		top_row = stl[stl[,"y"]==y_positions[length(y_positions)], ]
+	)
+
+
+
+	for(margin_index in seq_along(outer_margins)) {
+		stl_slice <- outer_margins[[margin_index]]
+		stl_slice <- stl_slice[order(stl_slice[,"x"], stl_slice[,"y"], decreasing=FALSE),] # make sure in order
+		for (depth_index in seq_along(depth_passes)) {
+			stl_slice <- stl_slice[rev(sequence(nrow(stl_slice)))] # so we go back and forth
+			desired_z <- depth_passes[depth_index]
+			valid_indices <- which(stl_slice[,"z"]<=desired_z)
+			previous_point <- NA
+			if(length(valid_indices)>0) {
+				cat(paste0("G1 Z", stepover_height, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #let's slowly go up to clear the piece
+				cat(paste0("G0 X", stl_slice[1,"x"], " Y", stl_slice[1,"y"], "\n"), file=gcode_file, append=TRUE) #shoot over to the next position where we will plunge for this cut
+				cat(paste0("G1 Z", desired_z, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #plunge to start the cut
+				for (position_index in seq_along(valid_indices)) {
+					if(position_index==1) {
+						cat(paste0("G0 X", stl_slice[valid_indices[position_index],"x"], " Y", stl_slice[valid_indices[position_index],"y"], "\n"), file=gcode_file, append=TRUE) #shoot over to the next position where we will plunge for this row
+						cat(paste0("G1 Z", desired_z, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #plunge to start the cut
+
+					} else {
+						if(abs(previous_point - valid_indices[position_index]) != 1) { #we have a gap before this point, so finish the previous cut, move up and over, and back in. This works if we go forward or backward with y
+							cat(paste0("G1 X", stl_slice[previous_point,"x"], " Y", stl_slice[previous_point,"y"], " F", horizontal_speed, "\n"), file=gcode_file, append=TRUE) #drive over to the previous point from the plunge earlier
+							cat(paste0("G1 Z", stepover_height, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #let's slowly go up to clear the piece
+							cat(paste0("G0 X", stl_slice[valid_indices[position_index],"x"], " Y", stl_slice[valid_indices[position_index],"y"], "\n"), file=gcode_file, append=TRUE) #shoot over to the next position where we will plunge for this row
+							cat(paste0("G1 Z", desired_z, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #plunge to start the cut
+						}
+					}
+					previous_point <- valid_indices[position_index]
+
+				}
+				cat(paste0("G1 X", stl_slice[valid_indices[position_index],"x"], " Y", stl_slice[valid_indices[position_index],"y"], " F", horizontal_speed, "\n"), file=gcode_file, append=TRUE) #mill over to the last point
+				cat(paste0("G1 Z", stepover_height, " F", vertical_speed, "\n"),  file=gcode_file, append=TRUE) #let's slowly go up to clear the piece
+			}
+		}
+	}
+
+	# The above loop will not do the very bottom of the cut on the margins -- will wait for final detail cut
+
 	for (depth_index in seq_along(depth_passes)) {
 		desired_z <- depth_passes[depth_index]
 
 		if(verbose) {
 			print(paste0("Now doing milling at ", depth_index, " steps down, ",desired_z, " ", unit, " into the piece."))
 		}
-		for (x_index in seq_along(x_positions)) {
+
+		
+
+		for (x_index in 2:(length(x_positions)-1)) { # we already did the left and right columns with the margin cuts
 			x_carve_position <- x_positions[x_index]
 			stl_slice <- stl[stl[,"x"]==x_positions[x_index],]
 			stl_slice <- stl_slice[order(stl_slice[,"y"], decreasing=FALSE),]
